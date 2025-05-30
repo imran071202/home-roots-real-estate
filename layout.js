3e64087979010d0a8ee61a1b11ed138bc79b7052
Imran Shaikh <168895222+imran071202@users.noreply.github.com>
2025-05-30 14:11:53 +0530

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,52 @@
+
+import Navbar from "@/components/Navbar";
+import Connect from '@/components/Connect'
+import Footer from "@/components/Footer";
+import { ToastContainer} from 'react-toastify';
+
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Home Roots",
+  description: "Real estate Company",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        /></head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased lightMode`}>
+        <Navbar/>
+        {children}
+              <ToastContainer position="top-right" autoClose={5000} />
+
+        <Connect/>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ page.js
@@ -0,0 +1,35 @@
+"use client"
+import React from 'react'
+import 'react-toastify/dist/ReactToastify.css';
+
+import Navbar from '@/components/Navbar'
+import Contain from '@/components/Contain'
+import Review from '@/components/Review'
+import About from '@/components/About';
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+// import Agent from '@/app/Agent'
+
+
+
+
+
+const page = () => {
+  return (
+    <>
+
+      
+      <Contain />
+      <About />
+      <Review />
+
+
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default page
