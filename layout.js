93e0a8979688f5a911db0112277199d4fec7a111
Imran Shaikh <168895222+imran071202@users.noreply.github.com>
2025-05-30 14:15:29 +0530

    Add files via upload

--- layout.js
+++ layout.js
@@ -6,8 +6,7 @@ import { ToastContainer} from 'react-toastify';
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
